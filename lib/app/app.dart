1512333f6ccbb4cf256d6103a8db8554063bfdc4
Marcio Coelho <marciocoelho@gmail.com>
2021-09-27 21:57:11 -0300

    Updated app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 
+import "package:imc/app/global/global.dart" as app;
 import "package:imc/app/view/home_view.dart";
 import "package:imc/app/view/i_m_c_view.dart";
 import "package:imc/app/view/about_view.dart";
@@ -8,20 +9,21 @@ class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Índice de Massa Corporal",
-      theme: ThemeData(
-        colorScheme: const ColorScheme.dark(),
-        fontFamily: "Montserrat",
-      ),
-      locale: const Locale("pt", "BR"),
-      routes: {
-        "home": (context) => const HomeView(),
-        "imc": (context) => const IMCView(),
-        "about": (context) => const AboutView(),
-      },
-      initialRoute: "home",
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: "Índice de Massa Corporal",
+        theme: ThemeData(
+          colorScheme: const ColorScheme.dark(
+            primary: app.primaryColor,
+            onPrimary: Colors.white,
+          ),
+          fontFamily: "Montserrat",
+        ),
+        locale: const Locale("pt", "BR"),
+        routes: {
+          "home": (context) => const HomeView(),
+          "imc": (context) => const IMCView(),
+          "about": (context) => const AboutView(),
+        },
+        initialRoute: "home",
+      );
 }
