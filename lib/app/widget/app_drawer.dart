785b733167292855272c630d605930cbc101fc56
Marcio Coelho <marciocoelho@gmail.com>
2021-09-27 21:59:05 -0300

    Updated app_drawer.dart

--- lib/app/widget/app_drawer.dart
+++ lib/app/widget/app_drawer.dart
@@ -1,47 +1,52 @@
 import "package:flutter/material.dart";
 
-import "app_list_tile.dart";
+import "package:imc/app/global/global.dart" as app;
+import "package:imc/app/widget/app_list_tile.dart";
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          DrawerHeader(
-            margin: EdgeInsets.zero,
-            padding: EdgeInsets.zero,
-            child: Stack(
-              children: [
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Image.asset("assets/img/imc.png"),
+  Widget build(BuildContext context) => Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              margin: EdgeInsets.zero,
+              padding: EdgeInsets.zero,
+              child: Stack(
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Image.asset("assets/img/imc.png"),
                     ),
-                ),
-                Positioned(
-                  bottom: 4,
-                  left: 16,
-                  child: Text(
-                    "Menu".toUpperCase(),
-                    style: const TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w800,
-                        letterSpacing: 2),
                   ),
-                ),
-              ],
+                  Positioned(
+                    bottom: 4,
+                    left: 16,
+                    child: app.text(string: "Menu", isTitle: true),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const AppListTile(Icons.home_outlined, "Home", "home"),
-          const AppListTile(Icons.monitor_weight_outlined, "IMC", "imc",
-              subTitle: "Índice de Massa Corporal"),
-          const Divider(),
-          const AppListTile(Icons.feed_outlined, "Sobre", "about"),
-        ],
-      ),
-    );
-  }
+            const AppListTile(
+              icon: Icons.home_outlined,
+              title: "Home",
+              link: "home",
+            ),
+            const AppListTile(
+              icon: Icons.monitor_weight_outlined,
+              title: "IMC",
+              link: "imc",
+              subTitle: "Índice de Massa Corporal",
+            ),
+            const Divider(),
+            const AppListTile(
+              icon: Icons.feed_outlined,
+              title: "Sobre",
+              link: "about",
+            ),
+          ],
+        ),
+      );
 }
