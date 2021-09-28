4e570b0fdbceadf9d805e50d7f2c1874c7381c66
Marcio Coelho <marciocoelho@gmail.com>
2021-09-27 21:59:18 -0300

    Updated app_list_tile.dart

--- lib/app/widget/app_list_tile.dart
+++ lib/app/widget/app_list_tile.dart
@@ -1,44 +1,51 @@
 import "package:flutter/material.dart";
 
+import "package:imc/app/global/global.dart" as app;
+
 class AppListTile extends StatelessWidget {
   final IconData icon;
   final String title;
   final String link;
-  final String subTitle;
+  final String? subTitle;
 
-  const AppListTile(
-    this.icon,
-    this.title,
-    this.link, {
+  const AppListTile({
     Key? key,
-    this.subTitle = "",
+    required this.icon,
+    required this.title,
+    required this.link,
+    this.subTitle,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Row(
-        children: [
-          Icon(icon),
-          Padding(
-            padding: const EdgeInsets.only(left: 8),
-            child: Text(title.toUpperCase(),
-                style: const TextStyle(color: Colors.white, letterSpacing: 2)),
-          ),
-        ],
-      ),
-      subtitle: subTitle != ""
-          ? Text(subTitle, style: const TextStyle(color: Colors.white))
-          : null,
-      onTap: () {
-        if (link != ModalRoute.of(context)!.settings.name)
-          Navigator.pushReplacementNamed(context, link);
-        else
-          Future.delayed(const Duration(milliseconds: 100),
-              () => Scaffold.of(context).openEndDrawer());
-      },
-      selected: link == ModalRoute.of(context)!.settings.name,
-      selectedTileColor: const Color(0xFF1EA1D9),
-    );
-  }
+  Widget build(BuildContext context) => ListTile(
+        title: Row(
+          children: [
+            Icon(icon),
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Text(
+                title.toUpperCase(),
+                style: const TextStyle(color: Colors.white, letterSpacing: 2),
+              ),
+            ),
+          ],
+        ),
+        subtitle: subTitle != null
+            ? Text(
+                subTitle!,
+                style: const TextStyle(color: Colors.white),
+              )
+            : null,
+        onTap: () {
+          if (link != ModalRoute.of(context)!.settings.name)
+            Navigator.pushReplacementNamed(context, link);
+          else
+            Future.delayed(
+              const Duration(milliseconds: 100),
+              () => Scaffold.of(context).openEndDrawer(),
+            );
+        },
+        selected: link == ModalRoute.of(context)!.settings.name,
+        selectedTileColor: app.primaryColor,
+      );
 }
